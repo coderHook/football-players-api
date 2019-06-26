d89bb324fee1a1c5f30d3c9bc1e429f05054f268
coderhook <abel1987@hotmail.es>
2019-06-26 12:46:03 +0200

    Added a route to post new teams

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const db = require('./db')
 const Team = require('./team/model.js')
 const teamRouter = require('./team/router')
+const bodyParser = require('body-parser')
 
 const express = require('express')
+const jsonParser = bodyParser.json()
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(jsonParser)
 app.use(teamRouter)
 
 
--- team/router.js
+++ team/router.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const Team = require('./model')
+// const bodyParser = require('body-parser')
 
 let router = express.Router()
+
+// router.use(bodyParser);
 
 router.get('/team', (req, res, next) => {
   Team.findAll()
       .then(teams => res.status(200).send(teams))
+      .catch(err => res.status(500).next(err))
+})
+
+router.get('/x')
+
+router.post('/team', (req, res, next) => {
+  Team.create(req.body)
+      .then(newTeam => res.status(200).send(newTeam))
       .catch(err => res.status(500).send(next(err)))
 })
 
