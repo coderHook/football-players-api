be681f4999e889bab7e186438915a0a71390d7ab
coderhook <abel1987@hotmail.es>
2019-06-26 12:01:15 +0200

    Added model Team

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const db = require('./db')
+const Team = require('./team/model.js')
 
 const express = require('express')
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 
 
 app.listen(port, () => `Listening on port ${port}`)
